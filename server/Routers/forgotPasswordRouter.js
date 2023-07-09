8354a2a671b60abdb85bbbf0f5b243d62293ae94
Senayldz <senasenayildiz@gmail.com>
2023-07-09 21:24:36 +0300

    .

--- server/Routers/forgotPasswordRouter.js
+++ server/Routers/forgotPasswordRouter.js
@@ -1,41 +1,23 @@
 import express from "express";
-import User from "../models/User"; // Kullanıcı modelini dahil edin
-import ForgotPassword from "../models/ForgotPassword"; // Şifre sıfırlama modelini dahil edin
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-router.post("/forgot-password", async (req, res) => {
+router.post("/password", async (req, res) => {
   const { email } = req.body;
 
   try {
-    // Kullanıcıyı e-posta adresine göre bulun
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      return res.status(404).json({ message: "Kullanıcı bulunamadı" });
-    }
-
-    // Şifre sıfırlama işlemi için bir token oluşturun
-    const resetToken = generateResetToken();
+    // Şifre sıfırlama bağlantısı için bir token oluşturun
 
     // Şifre sıfırlama verilerini veritabanına kaydedin
     await ForgotPassword.create({ email, resetToken });
 
-    // Şifre sıfırlama bağlantısını e-posta ile kullanıcıya gönderin
 
-    res
-      .status(200)
-      .json({ message: "Şifre sıfırlama bağlantısı e-posta adresinize gönderildi." });
+    res.status(200).json({ message: "Şifre sıfırlama bağlantısı e-posta adresinize gönderildi." });
   } catch (error) {
     res.status(500).json({ error: "Bir hata oluştu." });
   }
 });
 
-function generateResetToken() {
-  // Token oluşturma işlemleri burada gerçekleştirilecek
-  // Örneğin, UUID gibi bir kütüphane kullanabilirsiniz
-  const token = generateUUID(); // UUID oluşturma fonksiyonu
-  return token;
-}
 
 export default router;
