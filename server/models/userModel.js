434cd222bfb86cbcf1b7f0df6d25853ef865c4e1
Senayldz <senasenayildiz@gmail.com>
2023-07-10 02:00:41 +0300

    .

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema({
     },
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("user", userSchema);
 
 const validate = (user) => {
     const schema = Joi.object({
