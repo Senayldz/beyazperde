d28f969a1d2aff294df685dd073ea9d75292848e
Senayldz <senasenayildiz@gmail.com>
2023-07-10 00:57:59 +0300

    .

--- server/Routers/passwordReset.js
+++ server/Routers/passwordReset.js
@@ -2,10 +2,10 @@ import { User } from "../models/userModel.js";
 import Token from "../models/token.js";
 import sendEmail from "../utils/sendEmail.js";
 import crypto from "crypto";
-import Joi from "joi";
+import * as Joi from "joi";
 import express from "express";
 
-const router = express.Router();//joi eklendi...
+const router = express.Router();
 
 router.post("/", async (req, res) => {
     try {
