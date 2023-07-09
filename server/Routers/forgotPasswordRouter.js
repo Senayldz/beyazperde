65c0d8908dc695b4c65024618869bde6c5b991f4
Senayldz <senasenayildiz@gmail.com>
2023-07-09 17:24:29 +0300

    again

--- server/Routers/forgotPasswordRouter.js
+++ server/Routers/forgotPasswordRouter.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
-import { sendResetPasswordEmail } from "../emailService.js"; // E-posta gönderme servisi
 
 const router = express.Router();
 
@@ -9,13 +8,10 @@ router.post("/password", async (req, res) => {
 
   try {
     // Şifre sıfırlama bağlantısı için bir token oluşturun
-    const resetToken = generateResetToken();
 
     // Şifre sıfırlama verilerini veritabanına kaydedin
     await ForgotPassword.create({ email, resetToken });
 
-    // Şifre sıfırlama bağlantısını e-posta ile kullanıcıya gönderin
-    await sendResetPasswordEmail(email, resetToken); // E-posta gönderme fonksiyonu
 
     res.status(200).json({ message: "Şifre sıfırlama bağlantısı e-posta adresinize gönderildi." });
   } catch (error) {
@@ -23,10 +19,5 @@ router.post("/password", async (req, res) => {
   }
 });
 
-function generateResetToken() {
-  // UUID kütüphanesini kullanarak rastgele bir token oluşturun
-  const token = uuidv4();
-  return token;
-}
 
 export default router;
