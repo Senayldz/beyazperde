88e548fe52cd1ccd74a7dd5bd2edff7c3b6e63ed
Senayldz <senasenayildiz@gmail.com>
2023-07-10 13:30:06 +0300

    .........

--- client/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import {render,screen} from '@testing-library/react';
-
-import App from './App';
-test('Header renders correctly',()=>{
-    render(<App />);
-    const headerEl=screen.getByPlaceholderText('Ara')
-   expect(headerEl).toBeInDocument();
-})
--- client/src/Screens/HomeScreen.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Newses from "./Newses";
-
-const HomeScreen=({user})=>{
-  return <div>{user?.email}
-  <Newses/></div>;
-};
-
-export default HomeScreen;
